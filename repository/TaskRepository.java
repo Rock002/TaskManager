ae16ebbbd8061cdbd42c927f4300ee93de663128
Rock002 <chaykhoyan@mail.ru>
2025-06-22 21:37:33 +0300

    Delete repository directory

--- repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.TaskManager.repository;
-
-
-import com.example.TaskManager.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
